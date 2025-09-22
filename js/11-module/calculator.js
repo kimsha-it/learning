3f889d0949452fe3f5463e1d95b8a5b02523fe79
kimsha-it <kimsha0it@gmail.com>
2025-09-22 11:48:57 +0900

    js 모듈

---
+++ js/11-module/calculator.js
@@ -0,0 +1,29 @@
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// // 하나만 내보내기
+// export default add;
+
+// // 여러개 내보내기
+// // export { add, substract, ...}
+
+// 가장 기본적인 형태의 하나 내보내기(Default Export) 방식
+// 여러개의 함수(메서드)가 정의된 객체를 생성해서 내보내기
+
+// 객체란 중괄호로 표현되는 속성의 모음
+const calculator = {
+  add: function (n1, n2) {
+    return n1 + n2;
+  },
+  // substract 함수
+  substract: function (n1, n2) {
+    return n1 - n2;
+  },
+  // multiply 화살표 함수
+  multiplyArrow: (n1, n2) => {
+    return n1 * n2;
+  },
+};
+// 하나만 내보내기(Default Export) 코드 작성
+export default calculator;
