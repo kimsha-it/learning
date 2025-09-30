87b2819b69569025569d41487ea08a1fa865ed73
kimsha-it <kimsha0it@gmail.com>
2025-09-30 14:56:01 +0900

    clear

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Container from "./components/List/Container";
 
+
 export default function App() {
   return (
     <div>
-      <Container></Container>
+      {/* <Container></Container> */}
     </div>
   );
 }
