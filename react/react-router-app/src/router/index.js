120397682ef4cfab4087c4c92f7113353394aad6
kimsha-it <kimsha0it@gmail.com>
2025-10-13 14:52:50 +0900

    react-nested

--- react/react-router-app/src/router/index.js
+++ react/react-router-app/src/router/index.js
@@ -4,24 +4,59 @@
 import { createBrowserRouter } from "react-router-dom";
 
 // 페이지 컴포넌트 불러오기
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Profile from "../pages/Profile";
+import Home from "../pages/RootPages/Home.jsx";
+import About from "../pages/RootPages/About.jsx";
+import Profile from "../pages/RootPages/Profile.jsx";
+
+// 레이아웃 컴포넌트 불러오기
+import RootLayout from "../layout/RootLayout";
+import AuthLayout from "../layout/AuthLayout.jsx";
+
+import AuthHome from "../pages/AuthPages/AuthHome.jsx";
+import Signup from "../pages/AuthPages/Signup.jsx";
+import Login from "../pages/AuthPages/Login.jsx";
 
 // 라우터 생성
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Home,
-  },
-  {
-    path: "/about",
-    Component: About,
+    Component: RootLayout,
+    children: [
+      // 중첩할 자식 경로 객체를 정의하는 배열
+      {
+        index: true, // index: true -> 부모 경로의 기본 경로
+        // 완성되는 path: "/" (부모 경로와 동일)
+        Component: Home,
+      },
+      {
+        path: "about",
+        Component: About,
+      },
+
+      {
+        path: "profile",
+        Component: Profile,
+      },
+    ],
   },
 
   {
-    path: "/profile",
-    Component: Profile,
+    path: "/auth",
+    Component: AuthLayout,
+    children: [
+      {
+        index: true,
+        Component: AuthHome,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "signup",
+        Component: Signup,
+      },
+    ],
   },
 ]);
 
